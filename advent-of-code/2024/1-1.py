501d07670f8782b258882c710c05e2f35c26a084
Robbie <73069287+rdkieran@users.noreply.github.com>
2024-12-01 23:11:52 +0000

    day 1 advent of code

---
+++ advent-of-code/2024/1-1.py
@@ -0,0 +1,14 @@
+with open("advent-of-code/2024/input/1.txt","r") as i:
+    list1, list2 = [], []
+    answer = 0
+
+    for line in i:
+        list1.append(int(line.strip().split("   ")[0]))
+        list2.append(int(line.strip().split("   ")[1]))
+    
+    list1, list2 = sorted(list1), sorted(list2)
+    
+    for n in range(len(list1)):
+        answer += abs(list1[n]-list2[n])
+
+    print(answer)
---
+++ advent-of-code/2024/1-2.py
@@ -0,0 +1,17 @@
+with open("advent-of-code/2024/input/1.txt","r") as inp:
+
+    list1, list2 = [], []
+    answer = 0
+
+    for line in inp:
+        list1.append(int(line.strip().split("   ")[0]))
+        list2.append(int(line.strip().split("   ")[1]))
+    
+    for nL in list1:
+        count = 0
+        for rL in list2:
+            if nL == rL:
+                count += 1
+        answer += nL*count
+
+    print(answer)
